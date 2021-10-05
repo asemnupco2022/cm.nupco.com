19cfd6ea2d095d05debcb045fb35b56ad8f72c4b
skyviewArif <89638761+skyviewArif@users.noreply.github.com>
2021-10-05 13:45:43 +0530

    commit 1.3

--- app/Console/Commands/ImportSap.php
+++ app/Console/Commands/ImportSap.php
@@ -38,7 +38,7 @@ class ImportSap extends Command
      */
     public function handle()
     {
-        Storage::disk('public_uploads')->put('sap_nupco_backup.csv', Storage::disk('nupco_remote')->get('PO Download_ZMM001_20211003_084814.csv'));
+        Storage::disk('public_uploads')->put('uploads/sap_nupco_backup.csv', Storage::disk('nupco_remote')->get('PO Download_ZMM001_20211003_084814.csv'));
 
         return 0;
     }
